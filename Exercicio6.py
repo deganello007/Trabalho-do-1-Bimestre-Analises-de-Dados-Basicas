9ccd92e23c0a76abd59702e4956613b333644118
Deganello <degaleeh1504@gmail.com>
2024-04-12 19:37:56 -0300

    correção dia 12-04-2024 em aula

--- Exercicio6.py
+++ Exercicio6.py
@@ -4,21 +4,11 @@ import matplotlib.pyplot as plt
 
 df = pd.read_csv("employee_data.csv")
 
-numeric_df = df.select_dtypes(include=['int64', 'float64'])
-
-correlation_matrix = numeric_df.corr()
-
-correlation_matrix = correlation_matrix.mask(pd.DataFrame(np.eye(correlation_matrix.shape[0], dtype=bool), index=correlation_matrix.index, columns=correlation_matrix.columns))
-
-max_correlation_pair = correlation_matrix.abs().stack().idxmax()
-
-column1 = max_correlation_pair[0]
-column2 = max_correlation_pair[1]
 
 plt.figure(figsize=(8, 6))
-plt.scatter(df[column1], df[column2], alpha=0.5)
-plt.title(f'Gráfico de Dispersão entre {column1} e {column2}')
-plt.xlabel(column1)
-plt.ylabel(column2)
+plt.scatter(df["YearsOfExperience"], df["GrossSalary"], alpha=0.5)
+plt.title(f'Gráfico de Dispersão entre YearsOfExperience e GrossSalary')
+plt.xlabel("YearsOfExperience")
+plt.ylabel("GrossSalary")
 plt.grid(True)
 plt.show()
